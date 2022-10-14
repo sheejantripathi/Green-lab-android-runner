8bd443e386556cd31a1871dee069f7268557d1b7
Shekhar Suman <shekhar.suman396@gmail.com>
2022-10-14 22:22:21 +0200

    Experimental Configurations for Trepn

--- examples/batterystats/Scripts/interaction.py
+++ examples/batterystats/Scripts/interaction.py
@@ -1,5 +1,6 @@
 # noinspection PyUnusedLocal
 def main(device, *args, **kwargs):
-    print('=INTERACTION=')
-    print((device.id))
-    print((device.current_activity()))
+    # print('=INTERACTION=')
+    # print((device.id))
+    # print((device.current_activity()))
+    pass
